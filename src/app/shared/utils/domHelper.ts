fc2b0b8341f60c4f5105cd45a5811fa986a8c4bb
Joel Gillette <joelliuscaesar@gmail.com>
2020-12-15 08:05:01 -0500

    added some comments and fixed some edge cases

--- src/app/shared/utils/domHelper.ts
+++ src/app/shared/utils/domHelper.ts
@@ -1,23 +1,38 @@
 export function focusNext(current: Element): void {
+	/*
+		I was trying to use this in response to pressing enter on a 
+		certain control. But for reasons, I don't quite understand
+		the keypress event is apparently something you can get caught in the
+		middle of so it was firing the keypress action for the next control
+		after it had been focused. So, we're using setTimeout here
+		to toss our work to the end of the queue so we focus, after the
+		key event is well and over with
+
+		#bug: explicit tab order is ignored
+	*/
 	setTimeout(() => {
-		const selectors = 'input, [tabindex], button, select, textarea'
+		const selectors = 'input, [tabindex], button, select, textarea';
 		const nodes = document.querySelectorAll(selectors);
-		console.log(nodes);
-		console.log(current);
-		let idx = 0;
-		for(; idx < nodes.length; idx++) {
-			if(nodes[idx] === current) {
-				idx++;
-				break;
+		//go back to first element
+		if(current === nodes[nodes.length -1]) {
+			const node = nodes[0] as HTMLElement;
+			//the disabled part is not fully tested
+			if(node.tabIndex !== -1 || !node.attributes['disabled']) {
+				node.focus();
+				return;
 			}
 		}
+		let idx = 0;
+		for(; idx < nodes.length; idx++) {
+			if(nodes[idx] === current) break;
+		}
+		idx++;
 		for(; idx < nodes.length; idx++) {
 			const node = nodes[idx] as HTMLElement;
+			//the disabled part is not fully tested
 			if(node.tabIndex === -1 || node.attributes['disabled']) {
 				continue;
 			}
-			console.log('go focus');
-			console.log(node);
 			node.focus();
 			break;
 		}
